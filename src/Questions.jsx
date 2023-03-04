a913335fde0951abe5776941c9c60b3d438f0e3b
Ashwini <ashwiniparaye9309@gmail.com>
2023-03-04 16:12:01 +0530

    modified

--- src/Questions.jsx
+++ src/Questions.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 function Questions() {
 
@@ -6,14 +6,25 @@ function Questions() {
 const apiGet = () => {
   fetch('https://0h8nti4f08.execute-api.ap-northeast-1.amazonaws.com/getQuestionDetails/getquestiondetails?QuestionID=AreaUnderTheCurve_2')
   .then((response) => response.json())
-  .then((data) => {
-    console.log('data===========', data)
+  .then((apiData) => {
+    console.log('data===========', apiData)
+
+    
+    for (let i=0; i < apiData.length; i++){
+        // console.log("========", apiData[i].Question)
+       
+    const QuestionText = apiData[i].Question;
+
+    console.log("QuestionText=====",QuestionText)
+        
+    }
+
   })
     
 }
   return (
     <>
-        <button onClick={apiGet}>Get Question</button>
+       <button onClick={apiGet}>Get Question</button>
     </>
   )
 }
